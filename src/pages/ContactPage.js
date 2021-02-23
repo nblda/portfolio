1bc1fc27de82bbfe4f1246b5127f132ef40f57af
nblda <nicolas.blda@gmail.com>
2021-02-23 14:11:40 +0100

    contactPage: adding reset form button and info send/not sent

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+        // import Button from 'react-bootstrap/Button';
 import emailjs from 'emailjs-com';
 
 import Hero from '../components/Hero';
@@ -10,119 +10,107 @@ import NavArrow from '../components/NavArrow';
 
 // import Axios from 'axios';
 
-function ContactPage(props) {
+class ContactPage extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         name: '',
-    //         email: '',
-    //         message: '',
-    //         disabled: false,
-    //         emailSent: null,
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            disabled: false,
+            emailSent: null
+        }
+    }
 
+    resetForm = () => {
+        // e.target.reset();
+        console.log('reset');
+        this.setState({
+            name: '',
+            email: '',
+            message: ''
+        });
+    }
+    
+    handleChange = (event) => {
+        console.log(event);
 
-    // handleChange = (event) => {
-    //     const target = event.target;
-    //     const value = target.type === 'checkbox' ? target.checked : target.value;
-    //     const name = target.name;
+        const target = event.target;
+        const value =  target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
 
-    //     this.setState({
-    //         [name]: value
-    //     })
-    // }
-
-
-    // handleSubmit = (event) => {
-    //     event.preventDefault();
-
-    //     console.log(event.target);
-
-    //     this.setState({
-    //         disabled: true
-    //     });
-
-    //     Axios.post('http://localhost:3030/api/email', this.state)
-    //         .then(res => {
-    //             if(res.data.success) {
-    //                 this.setState({
-    //                     disabled: false,
-    //                     emailSent: true
-    //                 });
-    //             } else {
-    //                 this.setState({
-    //                     disabled: false,
-    //                     emailSent: false
-    //                 });
-    //             }
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-
-    //             this.setState({
-    //                 disabled: false,
-    //                 emailSent: false
-    //             });
-    //         })
-
-    // }
-
-
-    // render() {
-
-    function sendEmail(e){
+    sendEmail = (e) => {
         e.preventDefault();
 
+        this.setState({
+            disabled: true
+        });
+
         //remplacer les valeurs apres inscription puis creation d'un service et d'un template
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+         emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
         .then((result) => {
             console.log(result.text);
+            this.setState({
+                disabled: false,
+                emailSent: true
+            });
         }, (error) => {
             console.log(error.text);
+            this.setState({
+                disabled: false,
+                emailSent: false
+            });
         });
-        e.target.reset();
     }
+    render(){
         return(
             <div>
-                <Hero title={props.title} />
+                <Hero title={this.props.title} />
 
                 <NavArrow side="left" to="/about"/>
 
                 <Content>
                     {/* this.handleSubmit */}
-                    <Form onSubmit={sendEmail}> 
+                    <Form onSubmit={this.sendEmail}> 
                         <Form.Group>
                             <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                            <Form.Control id="full-name" name="name" type="text" />
+                            <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
                         </Form.Group>
 
 
                         <Form.Group>
                             <Form.Label htmlFor="email">Email</Form.Label>
-                            <Form.Control id="email" name="email" type="email" />
+                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange}/>
                         </Form.Group>
 
 
                         <Form.Group>
                             <Form.Label htmlFor="message">Message</Form.Label>
-                            <Form.Control id="message" name="message" as="textarea"rows="3" />
+                            <Form.Control id="message" name="message" as="textarea"rows="3" value={this.state.message} onChange={this.handleChange}/>
                         </Form.Group>
 
 
-                        <Button className="d-inline-block" variant="primary" type="submit">
+                        <button className="btn m-1 btn-primary"  type="submit" disabled={this.state.disabled} >
                             Send
-                        </Button>
+                        </button>
+
+                        <button className="btn btn-outline-primary" type="button" onClick={this.resetForm}>
+                            Reset Form
+                        </button>
 
 
-                        {/* {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
-                        {this.state.emailSent === false && <p className="d-inline error-msg">Email Not Sent</p>} */}
+                        {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                        {this.state.emailSent === false && <p className="d-inline error-msg">Email Not Sent</p>}
                     </Form>
                 </Content>
             </div>
         );
-    // }
+    }
 
 }
 
